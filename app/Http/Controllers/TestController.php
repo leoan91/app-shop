371020e27bea2521f364bf483469e33246985603
Leonel <leoan91@gmail.com>
2018-10-14 21:56:16 -0600

    Base Project

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function welcome(){
+
+    	$a = 5;
+    	$b = 10;
+
+    	$c = $a + $b;
+
+    	return view('welcome');
+    }
+}
